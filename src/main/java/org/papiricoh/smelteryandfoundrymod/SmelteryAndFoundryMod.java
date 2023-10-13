364ed2a6065b7beb0d2f64f6af3d4dcf85806823
papiricoh <slendeman0200@gmail.com>
2023-10-13 11:53:07 +0200

    First Commit

---
+++ src/main/java/org/papiricoh/smelteryandfoundrymod/SmelteryAndFoundryMod.java
@@ -0,0 +1,13 @@
+package org.papiricoh.smelteryandfoundrymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SmelteryAndFoundryMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/papiricoh/smelteryandfoundrymod/client/SmelteryAndFoundryModClient.java
@@ -0,0 +1,13 @@
+package org.papiricoh.smelteryandfoundrymod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmelteryAndFoundryModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
