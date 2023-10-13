cff27957d8f668c2e3f40f9f151b22cd1bc69c17
papiricoh <slendeman0200@gmail.com>
2023-10-13 14:04:41 +0200

    Added MarbleBlock

--- src/main/java/org/papiricoh/smelteryandfoundrymod/SmelteryAndFoundryMod.java
+++ src/main/java/org/papiricoh/smelteryandfoundrymod/SmelteryAndFoundryMod.java
@@ -1,13 +1,23 @@
 package org.papiricoh.smelteryandfoundrymod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.papiricoh.smelteryandfoundrymod.blocks.MarbleBlock;
 
 public class SmelteryAndFoundryMod implements ModInitializer {
     /**
      * Runs the mod initializer.
      */
+    public static final Block MARBLE_BLOCK = new MarbleBlock(FabricBlockSettings.create().strength(4.0f));
     @Override
     public void onInitialize() {
-
+        Registry.register(Registries.BLOCK, new Identifier("saf", "marble_block"), MARBLE_BLOCK);
+        Registry.register(Registries.ITEM, new Identifier("saf", "marble_block"), new BlockItem(MARBLE_BLOCK, new Item.Settings()));
     }
 }
---
+++ src/main/java/org/papiricoh/smelteryandfoundrymod/blocks/MarbleBlock.java
@@ -0,0 +1,9 @@
+package org.papiricoh.smelteryandfoundrymod.blocks;
+
+import net.minecraft.block.Block;
+
+public class MarbleBlock extends Block {
+    public MarbleBlock(Settings settings) {
+        super(settings);
+    }
+}
