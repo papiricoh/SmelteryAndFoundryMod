4f642bc415aba16b17cc0b956eab475fef88a648
Papiricoh <slendeman0200@gmail.com>
2023-10-15 17:45:50 +0200

    Added marble in worldGen

--- src/main/java/org/papiricoh/smelteryandfoundrymod/SmelteryAndFoundryMod.java
+++ src/main/java/org/papiricoh/smelteryandfoundrymod/SmelteryAndFoundryMod.java
@@ -1,6 +1,8 @@
 package org.papiricoh.smelteryandfoundrymod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -8,8 +10,12 @@ import net.minecraft.block.Block;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.papiricoh.smelteryandfoundrymod.blocks.MarbleBlock;
 
 public class SmelteryAndFoundryMod implements ModInitializer {
@@ -19,6 +25,9 @@ public class SmelteryAndFoundryMod implements ModInitializer {
     public static final Block MARBLE_BLOCK = new MarbleBlock(FabricBlockSettings.create().strength(4.0f));
     public static final Block POLISHED_MARBLE_BLOCK = new MarbleBlock(FabricBlockSettings.create().strength(4.0f));
     public static final Block MARBLE_BRICKS_BLOCK = new MarbleBlock(FabricBlockSettings.create().strength(4.0f));
+
+    public static final RegistryKey<PlacedFeature> MARBLE_ORE = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("saf","marble_ore"));
+
 
     private static final ItemGroup ITEM_GROUP_BLOCKS = FabricItemGroup.builder()
             .icon(() -> new ItemStack(MARBLE_BLOCK))
@@ -40,6 +49,7 @@ public class SmelteryAndFoundryMod implements ModInitializer {
         Registry.register(Registries.BLOCK, new Identifier("saf", "marble_bricks_block"), MARBLE_BRICKS_BLOCK);
         Registry.register(Registries.ITEM, new Identifier("saf", "marble_bricks_block"), new BlockItem(MARBLE_BRICKS_BLOCK, new Item.Settings()));
 
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, MARBLE_ORE);
     }
 
 
